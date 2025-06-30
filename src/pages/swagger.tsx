add64588e448e612d46b8031f8dce886de60d426
RYE-513 <ryan13briz@gmail.com>
2025-06-30 21:42:46 +0800

    added Dockerfile

--- src/pages/swagger.tsx
+++ src/pages/swagger.tsx
@@ -1,5 +1,8 @@
-import SwaggerUi from "swagger-ui-react"
+import dynamic from "next/dynamic";
 import "swagger-ui-react/swagger-ui.css"
+
+// Dynamically import swagger-ui-react with SSR turned off
+const SwaggerUi = dynamic(() => import("swagger-ui-react"), { ssr: false });
 
 export default function SwaggerPage() {
     return <SwaggerUi url="/api/docs" />
